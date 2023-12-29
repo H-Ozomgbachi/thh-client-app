a9cc13a68e01cb0e9289c8717e4db760a04b2abc
Qudus Adeyemi <adeyemiqudus361@gmail.com>
2023-12-29 05:02:28 +0100

    Add logisfi-pro model

---
+++ src/api/models/logisfi-pro/vendor.ts
@@ -0,0 +1,14 @@
+export interface VendorData {
+  vendorCode: string;
+  name: string;
+  contactEmail: string;
+  contactPhone: string;
+  address: string;
+  city: string;
+  state: string;
+  accountName: any;
+  accountNumber: any;
+  bankName: any;
+  bankCode: any;
+  dateCreated: string;
+}
